69a72b8aac08aa335051d5fa923fb54e2544c827
itsLeonB <ellionblessan@gmail.com>
2026-09-21 10:20:01 +0700

    chore(frontend): enable spa mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,12 @@ import tailwindcss from "@tailwindcss/vite"
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart({ spa: { enabled: true } }),
+    viteReact(),
+  ],
   test: {
     environment: "jsdom",
   },
